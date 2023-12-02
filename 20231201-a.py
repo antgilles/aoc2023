638bc4f0213da335d0a69aeb69099ae73604665b
Antoine GILLES <antoine.gilles@algolia.com>
2023-12-02 12:20:04 +0100

    day2

--- 20231201-a.py
+++ 20231201-a.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python
-import math
 from aocd.models import Puzzle
 
 def firstdigit(word):
     for i in word:
-        #print(i)
         if i.isdigit():
             return (i)
     exit(1)
@@ -12,7 +10,6 @@ def firstdigit(word):
 def lastdigit(word):
     i = 1
     while i <= len(word):
-        #print(word[-i])
         if word[-i].isdigit():
             return (word[-i])
         else: i += 1
@@ -20,7 +17,6 @@ def lastdigit(word):
 puzzle = Puzzle(year=2023, day=1)
 result = 0
 for word in puzzle.input_data.split('\n'):
-    #print(firstdigit(word)+str(lastdigit(word)))
     result += int(firstdigit(word)+lastdigit(word))
 print(result)
 
--- 20231201-b.py
+++ 20231201-b.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python
-import math
 from aocd.models import Puzzle
 
 def preparestr(word):
